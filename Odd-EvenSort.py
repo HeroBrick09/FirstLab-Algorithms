fe59b5a60a9a2f2e5b6804e46d6b88351179b641
FABIO MARTIN MORALES FERNANDEZ <fabio.morales@ucr.ac.cr>
2025-08-19 20:27:07 -0600

    Fix variable naming consistency in oddEvenSortHuman function

--- Odd-EvenSort.py
+++ Odd-EvenSort.py
@@ -6,19 +6,19 @@ def main():
     runSortsNoPrint()
 
 def oddEvenSortHuman(a,n):
-    is_sorted=False
+    isSorted=False
 
-    while not is_sorted:
-        is_sorted=True
+    while not isSorted:
+        isSorted=True
 
         for i in range(1,n-2,2):
             if a[i] > a[i+1]:
                 a[i], a[i+1] = a[i+1], a[i]
-                is_sorted = False
+                isSorted = False
         for i in range(0,n-1,2):
             if a[i] > a[i+1]:
                 a[i], a[i+1] = a[i+1], a[i]
-                is_sorted = False
+                isSorted = False
     return a
 
 def odd_even_sort_ChatGpt(A, n):
